c27603c830a1dfbb544cdfad15c1962de645d750
Sam Daitzman <sam@daitzman.com>
2020-08-08 13:54:46 -0400

    v basic implementation of url middleware

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
+((fn) => {
+    if (document.readyState != 'loading') fn()
+    else document.addEventListener('DOMContentLoaded', fn)
+})(() => {
+    let params = Object.fromEntries(new URLSearchParams(location.search))
 
-// a utility function to get URL parameters
-function getUrlParameter(name) {
-    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]')
-    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)')
-    var results = regex.exec(location.search)
-    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '))
-}
-
-let fn = () => {
-    let cameFrom = getUrlParameter('from')
-    if (cameFrom) {
+    if (params.from) {
         let goBack = document.querySelector('.go-back')
+
         goBack.classList.remove('hidden')
-        goBack.innerText += ' to ' + cameFrom
-        goBack.href = cameFrom
-    }
-}
+        goBack.innerText += ' to ' + params.from
+        goBack.href = params.from
 
-let ready = (fn) => {
-    if (document.readyState != 'loading') {
-        fn()
-    } else {
-        document.addEventListener('DOMContentLoaded', fn);
-    }
-}
+        // parse sites list
+        if (params.to) {
+            var sites = [...document.querySelectorAll('.ring ol li a')]
+            var current_site = sites.findIndex(site => site.href === params.from)
+        }
 
-ready(fn)
+        if (params.to === 'next')
+            console.log(sites[(current_site + 1) % sites.length].href);
+        else if (params.to === 'prev') console.log(sites[(current_site - 1 + sites.length) % sites.length].href)
+
+    }
+})
