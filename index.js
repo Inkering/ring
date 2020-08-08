03448aafd8acf65b705f04aa0c90564b87aaa5bd
Sam Daitzman <sam@daitzman.com>
2020-08-08 14:56:34 -0400

    working redirect, next-site link, and back-link verification thanks @jdtzmn

--- index.js
+++ index.js
@@ -1,25 +1,80 @@
-((fn) => {
-    if (document.readyState != 'loading') fn()
+const normalizeUrl = require('normalize-url')
+window.normalizeUrl = normalizeUrl
+
+/**
+ * Remove the protocol from a url
+ * @param {{string}} url The url to remove the protocl from
+ */
+function withoutProtocol (url) {
+    return url.split('//').slice(1).join('')
+}
+
+function prettifyUrl (url) {
+    return withoutProtocol(normalizeUrl(url))
+}
+
+/**
+ * Compare whether two urls are equal after normalizing them
+ * @param {string} a The first url to compare
+ * @param {string} b The second url to compare
+ */
+function compareUrls (a, b) {
+    if (a === b) {
+        return true
+    }
+
+    return prettifyUrl(a) === prettifyUrl(b)
+}
+
+function onceReady (fn) {
+    if (document.readyState !== 'loading') fn()
     else document.addEventListener('DOMContentLoaded', fn)
-})(() => {
+}
+
+/**
+ * Returns [url, index]
+ * @param {*} from The from parameter 
+ */
+function validFromUrl (from) {
+    let sites = [...document.querySelectorAll('.ring ol li a')]
+    const current_site_anchor = sites.find(site => compareUrls(site.href, from))
+    return [current_site_anchor.href, sites.indexOf(current_site_anchor)]
+}
+
+function getPreviousSite(current_index) {
+    return getSiteRelative(current_index, -1)
+}
+
+function getNextSite(current_index) {
+    return getSiteRelative(current_index, 1)
+}
+
+function getSiteRelative(current_index, delta) {
+    let sites = [...document.querySelectorAll('.ring ol li a')]
+    return sites[(current_index + delta + sites.length) % sites.length].href
+}
+
+onceReady(() => {
     let params = Object.fromEntries(new URLSearchParams(location.search))
 
-    if (params.from) {
+    let [current_site, current_index] = validFromUrl(params.from)
+    if (current_site) {
         let goBack = document.querySelector('.go-back')
 
         goBack.classList.remove('hidden')
         goBack.innerText += ' to ' + params.from
-        goBack.href = params.from
+        goBack.href = current_site
 
-        // parse sites list
-        if (params.to) {
-            var sites = [...document.querySelectorAll('.ring ol li a')]
-            var current_site = sites.findIndex(site => site.href === params.from)
-        }
+        let goForward = document.querySelector('.go-forward')
+        const nextSite = getNextSite(current_index)
+        
+        goForward.classList.remove('hidden')
+        goForward.innerText = goForward.innerText.replace('$next', prettifyUrl(nextSite))
+        goForward.href = nextSite
 
         if (params.to === 'next')
-            console.log(sites[(current_site + 1) % sites.length].href);
-        else if (params.to === 'prev') console.log(sites[(current_site - 1 + sites.length) % sites.length].href)
+            window.location.href = (getNextSite(current_index));
+        else if (params.to === 'prev') window.location.href = getPreviousSite(current_index)
 
     }
 })
